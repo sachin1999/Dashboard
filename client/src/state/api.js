9eb5ce40b9eec89eaa07d697f9e6f20de0e92d16
Sachin Gupta <sachinjava20@gmail.com>
2023-11-18 00:09:25 +0530

    lastone

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,8 +1,8 @@
-const BASE_URL = 'http://localhost:4000'
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 const countryResponse = await fetch(`${BASE_URL}/country`);
 export const fetchedCountries = await countryResponse.json();
-//console.log(fetchedCountries);
+// console.log(fetchedCountries);
 
 const sectorResponse = await fetch(`${BASE_URL}/sectors`);
 export const fetchedSectors = await sectorResponse.json();
