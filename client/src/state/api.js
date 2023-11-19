0afe6acfccd80e8a973087490ac9e73c11e567e5
Sachin Gupta <sachinjava20@gmail.com>
2023-11-19 07:38:00 +0530

    ready

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL= "https://dashboard-r3ks.onrender.com/api"
+axios.defaults.baseURL= "https://dashboard-r3ks.onrender.com"
 const countryResponse = await axios.get(`/country`);
 export const fetchedCountries = await countryResponse.data;
 // console.log(fetchedCountries);
